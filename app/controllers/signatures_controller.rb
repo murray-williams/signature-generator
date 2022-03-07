cad09f0e1d049b7455b13b6f837e9ccd77b84759
Murray Williams <murray@waterfalldigital.co.za>
2022-03-07 21:45:56 +0200

    Setup create signature views and methods

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -8,7 +8,26 @@ class SignaturesController < ApplicationController
     @signature = Signature.find(params[:id])
   end
 
+  def new
+    @signature = Signature.new
+  end
+
+  def create
+    @signature = Signature.new(signature_params)
+
+    respond_to do |format|
+      if @signature.save
+        format.html { redirect_to signature_url(@signature), notice: "Signature was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
+  def signature_params
+    params.require(:signature).permit(:fullname, :email, :job_title)
+  end
 
 end
